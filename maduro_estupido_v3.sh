63fe413a9727d112252c699c7753c45578361ce8
GB <g.gonzalezsotillo@gmail.com>
2024-08-04 23:56:37 -0300

    Update maduro_estupido_v3.sh

--- maduro_estupido_v3.sh
+++ maduro_estupido_v3.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-  echo "You must enter exactly 3 numbers as arguments."
+# Check if cedulas.txt exists
+if [ ! -f cedulas.txt ]; then
+  echo "File cedulas.txt not found!"
   exit 1
 fi
 
-amount=$1
-min=$2
-max=$3
+# Create the directory for downloads if it doesn't exist
+mkdir -p actas
 
-for (( i=0; i<amount; i++ ))
-do
-    random_number=$(( ( RANDOM * 32768 + RANDOM ) % (max - min + 1) + min ))
-    base_url="https://gdp.theempire.tech/api/data?cdi=v${random_number}"
+# Read the file line by line
+while IFS= read -r cedula; do
+  base_url="https://gdp.theempire.tech/api/data?cdi=v${cedula}"
 
-    response=$(curl -s "$base_url")
-    
-    # Check if the response is {"error":"not-found"}
-    if [ "$response" == '{"error":"not-found"}' ]; then
-        echo "Data not found for V$random_number"
-        continue
+  response=$(curl -s "$base_url")
+
+  # Check if the response is {"error":"not-found"}
+  if [ "$response" == '{"error":"not-found"}' ]; then
+    echo "Data not found for V$cedula"
+    continue
+  fi
+
+  echo $response
+
+  # Extracting DO_DS_NAME and DO_DS_BUCKET from the response
+  do_ds_name=$(echo "$response" | jq -r '.acta.DO_DS_NAME')
+  do_ds_bucket=$(echo "$response" | jq -r '.acta.DO_DS_BUCKET')
+
+  if [ "$do_ds_name" != "null" ] && [ "$do_ds_bucket" != "null" ]; then
+    # Constructing the download URL
+    download_url="https://$do_ds_bucket.s3.amazonaws.com/$do_ds_name"
+    filename=$(basename "$do_ds_name")
+
+    # Check if the file already exists
+    if [ -f "./actas/$filename" ]; then
+      echo "File $filename already exists, skipping"
+      continue
     fi
 
-    echo $response
-
-    # Extracting DO_DS_NAME and DO_DS_BUCKET from the response
-    do_ds_name=$(echo "$response" | jq -r '.acta.DO_DS_NAME')
-    do_ds_bucket=$(echo "$response" | jq -r '.acta.DO_DS_BUCKET')
-
-    if [ "$do_ds_name" != "null" ] && [ "$do_ds_bucket" != "null" ]; then
-        # Constructing the download URL
-        download_url="https://$do_ds_bucket.s3.amazonaws.com/$do_ds_name"
-        filename=$(basename "$do_ds_name")
-
-        # Check if the file already exists
-        if [ -f "./actas2/$filename" ]; then
-            echo "File $filename already exists, skipping"
-            continue
-        fi
-
-        # Downloading the file
-        curl -s "$download_url" --output "./actas2/$filename"
-        echo "Downloaded $filename"
-    else
-        echo "Required data not found in the response for V$random_number"
-    fi
-done
+    # Downloading the file
+    curl -s "$download_url" --output "./actas/$filename"
+    echo "Downloaded $filename"
+  else
+    echo "Required data not found in the response for V$cedula"
+  fi
+done < cedulas.txt
